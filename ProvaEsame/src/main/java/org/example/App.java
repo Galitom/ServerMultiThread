944efa471eaebf23c4dfb0370fc167b98a6580c4
Tommaso Galimberti <t.galimberti@itsrizzoli.it>
2023-06-21 10:09:41 +0200

    Feat: ServerMultiThread with basic Handler

--- ProvaEsame/src/main/java/org/example/App.java
+++ ProvaEsame/src/main/java/org/example/App.java
@@ -1,28 +1,9 @@
 package org.example;
 
-import com.sun.net.httpserver.HttpServer;
-
-import java.io.IOException;
-
-import java.net.InetSocketAddress;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.*;
-
-
 public class App
 {
     public static void main( String[] args ) {
-        HttpServer server = null;
-        try {
-            server = HttpServer.create(new InetSocketAddress(8080), 0);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        server.createContext("/", new MyHandler());
-        server.setExecutor(null); // creates a default executor
-        server.start();
-
+        MultiEchoServer echoServer = new MultiEchoServer(1234);
+        echoServer.startServer();
     }
 }
